0642bc478e9eff443c9cd0f0e934e006bcf98349
Colin McDonald <1colinmcdonald@gmail.com>
2024-02-16 19:47:49 -0800

    Add operate function

--- script.js
+++ script.js
@@ -14,8 +14,25 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(operator, operand1, operand2) {
+  switch (operator) {
+    case "+":
+      return add(operand1, operand2);
+    case "-":
+      return subtract(operand1, operand2);
+    case "*":
+      return multiply(operand1, operand2);
+    case "/":
+      return divide(operand1, operand2);
+  }
+}
+
 let operand1;
 let operator;
 let operand2;
 
 console.log(divide(5, 0));
+console.log(operate("+", 6, 4));
+console.log(operate("*", 6, 4));
+console.log(operate("/", 6, 4));
+console.log(operate("-", 6, 4));
