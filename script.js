986f83ce477f1f23b64efff429df4a55bea16d90
Colin McDonald <1colinmcdonald@gmail.com>
2024-02-26 19:16:33 -0800

    Improve functionality

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let displayContent = document.querySelector("#displayContent");
-let operand1 = displayContent.textContent;
-let operator = null;
-let operand2 = null;
+let info = { operand1: null, operand2: null, operator: null };
 
 function add(a, b) {
   return a + b;
@@ -22,38 +20,44 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, operand1, operand2) {
-  if (operand2 == null) return operand1;
-  operand1 = Number(operand1);
-  operand2 = Number(operand2);
+function operate() {
+  clearSelectedOperator();
+  printInfo();
+  if (info.operand2 === null) return info.operand1;
+  if (info.operand1 === null) info.operand1 = displayContent.textContent;
+  workingOperand1 = Number(info.operand1);
+  workingOperand2 = Number(info.operand2);
   let result = null;
-  switch (operator) {
+  switch (info.operator) {
     case "+":
-      result = add(operand1, operand2);
+      result = add(workingOperand1, workingOperand2);
       break;
     case "-":
-      result = subtract(operand1, operand2);
+      result = subtract(workingOperand1, workingOperand2);
       break;
     case "*":
-      result = multiply(operand1, operand2);
+      result = multiply(workingOperand1, workingOperand2);
       break;
     case "/":
-      result = divide(operand1, operand2);
+      result = divide(workingOperand1, workingOperand2);
       break;
   }
+  info.operand1 = null;
+  info.operand2 = null;
+  info.operator = null;
+
   if (result === null) return "0";
+  printInfo();
   return result.toString().slice(0, 9);
 }
 
-function clear() {
+function handleClear() {
   displayContent.textContent = "0";
-  operand1 = displayContent.textContent;
-  operator = null;
-  operand2 = null;
+  info.operand1 = null;
+  info.operator = null;
+  info.operand2 = null;
+  clearSelectedOperator();
 }
-
-let AC = document.querySelector(".clear");
-AC.addEventListener("click", clear);
 
 let calculator = document.querySelector("#calculator");
 calculator.addEventListener("mouseover", (e) => {
@@ -70,86 +74,128 @@ calculator.addEventListener("click", (e) => {
 });
 
 window.addEventListener("keydown", (e) => {
-  input = e.key;
-  switch (input) {
-    case "c":
-      input = "AC";
-      break;
-    case "C":
-      input = "AC";
-      break;
-    case "d":
-      input = "D";
-      break;
-    case "Backspace":
-      input = "D";
-      break;
-    case "Delete":
-      input = "D";
-      break;
-    case "Enter":
-      input = "=";
-      break;
-  }
-  handleInput(input);
+  let options = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    c: "AC",
+    C: "AC",
+    a: "AC",
+    A: "AC",
+    d: "D",
+    D: "D",
+    Backspace: "D",
+    Delete: "D",
+    Enter: "=",
+    "=": "=",
+    "/": "/",
+    "*": "*",
+    "-": "-",
+    "+": "+",
+  };
+  let input = options[e.key];
+  console.log("handling input:");
+  printInfo();
+  handleInput(options[e.key]);
 });
 
 function handleInput(input) {
   if (!isNaN(input) || input === ".") {
-    if (operator === null) {
-      if (operand1 === "0" && !isNaN(input)) {
-        displayContent.textContent = input;
-      } else if (displayContent.textContent.length < 9) {
-        displayContent.textContent += input;
-      }
-      operand1 = displayContent.textContent;
-    } else {
-      if (operand2 === null) {
-        displayContent.textContent = input;
-      } else if (operand2 !== "0" && displayContent.textContent.length < 9) {
-        displayContent.textContent += input;
-      }
-      operand2 = displayContent.textContent;
-    }
+    handleData(input);
   } else if (input === "/" || input === "*" || input === "-" || input === "+") {
-    console.log(input);
-    let button = "";
-    switch (input) {
-      case "/":
-        button = "divide";
-        break;
-      case "*":
-        button = "multiply";
-        break;
-    }
-
-    if (operand2 !== null) {
-      displayContent.textContent = operate(operator, operand1, operand2);
-    }
-    operand1 = displayContent.textContent;
-    operand2 = null;
-    operator = input;
+    handleOperator(input);
   } else if (input === "=") {
-    displayContent.textContent = operate(operator, operand1, operand2);
-    operator = null;
-    operand1 = displayContent.textContent;
-    operand2 = null;
+    handleEquals();
   } else if (input === "D") {
-    if (operator !== null && operand2 !== null) {
-      displayContent.textContent = displayContent.textContent.slice(0, -1);
-      if (displayContent.textContent === "") {
-        displayContent.textContent = "0";
-      }
-      operand2 = displayContent.textContent;
-    }
-    if (operator == null) {
-      displayContent.textContent = displayContent.textContent.slice(0, -1);
-      if (displayContent.textContent === "") {
-        displayContent.textContent = "0";
-      }
-      operand1 = displayContent.textContent;
-    }
+    handleDelete();
   } else if (input === "AC") {
-    clear();
+    handleClear();
   }
+}
+function handleDelete() {
+  if (info.operator !== null && info.operand2 !== null) {
+    displayContent.textContent = displayContent.textContent.slice(0, -1);
+    if (displayContent.textContent === "") {
+      displayContent.textContent = "0";
+    }
+    info.operand2 = displayContent.textContent;
+  }
+  if (info.operator == null) {
+    displayContent.textContent = displayContent.textContent.slice(0, -1);
+    if (displayContent.textContent === "") {
+      displayContent.textContent = "0";
+    }
+    info.operand1 = displayContent.textContent;
+  }
+}
+
+function handleEquals() {
+  displayContent.textContent = operate();
+}
+
+function handleOperator(input) {
+  console.log(input);
+  let button = "";
+  switch (input) {
+    case "/":
+      button = "divide";
+      break;
+    case "*":
+      button = "multiply";
+      break;
+    case "-":
+      button = "subtract";
+      break;
+    case "+":
+      button = "add";
+      break;
+  }
+
+  let selected = document.querySelector("." + button);
+  clearSelectedOperator();
+  selected.style.backgroundColor = "rgb(102, 0, 51)";
+  if (info.operand2 !== null) {
+    displayContent.textContent = operate();
+  }
+  info.operator = input;
+}
+
+function clearSelectedOperator() {
+  let binaryOperators = document.querySelectorAll(".binaryOperator");
+  for (let i = 0; i < binaryOperators.length; i++) {
+    binaryOperators[i].style.backgroundColor = "rgb(172, 40, 53)";
+  }
+}
+
+function handleData(input) {
+  console.log("handling data:");
+  printInfo();
+
+  if (info.operator === null) {
+    if (info.operand1 === null && !isNaN(input)) {
+      displayContent.textContent = input;
+    } else if (displayContent.textContent.length < 9) {
+      displayContent.textContent += input;
+    }
+    info.operand1 = displayContent.textContent;
+  } else {
+    if (info.operand2 === null) {
+      displayContent.textContent = input;
+    } else if (info.operand2 !== "0" && displayContent.textContent.length < 9) {
+      displayContent.textContent += input;
+    }
+    info.operand2 = displayContent.textContent;
+  }
+}
+function printInfo() {
+  console.log("operand1: " + info.operand1);
+  console.log("operand2: " + info.operand2);
+  console.log("operator: " + info.operator);
 }
